78b7ebde75453ee5b66a4914b8920f542c2a9bfc
Richard Beith Jr <rbeith@gmail.com>
2022-03-10 13:10:52 -0800

    uploading to work on another machine

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
 
 
-    const container = document.querySelector('.container');
-    container.style.display = 'flex';
-    container.style.flexDirection = 'column';
+const container = document.querySelector('.container');
+container.style.display = 'flex';
+container.style.flex = '1, 0, auto'
+container.style.flexDirection = 'column';
+container.style.height = '600px';
+container.style.width = '600px';
+container.style.justifyContent = 'flex-start';
+container.style.alignItems = 'center'
 
-    for (i = 0; i < 16; i++) {    
+const button = document.createElement('button');
+container.appendChild(button);
+button.textContent = 'Reset';
+button.style.margin = '24px'
+
+const gridBox = document.createElement('gridBox');
+container.appendChild(gridBox);
+gridBox.style.display = 'flex';
+gridBox.style.flexDirection = 'column';
+gridBox.style.flex = '1, 0, auto';
+gridBox.style.height = '500px';
+gridBox.style.width = '500px';
+
+grid(); 
+function grid(size = 16) {
+    for (i = 0; i < size; i++) {    
         let row = document.createElement('div');
-        container.appendChild(row)    
+        gridBox.appendChild(row)    
         row.className = 'row'
         row.style.display = 'flex';
-        row.style.flex = '1';
+        row.style.flex = '1, 0, auto';
         row.style.flexDirection = 'row';
-        for (n = 0; n < 16; n++) {
+        row.style.height = '100%';
+        row.style.width = '100%'
+        row.style.justifyContent = 'center';
+        row.style.alignItems = 'center'
+        for (n = 0; n < size; n++) {
             let pixel = document.createElement('div');
             row.appendChild(pixel);
             pixel.className = 'pixel'
+            pixel.style.display = 'flex'
             pixel.style.backgroundColor = 'black';
-            pixel.style.height = '20px';
-            pixel.style.width = '20px';
-            pixel.style.flex = '1';
-            pixel.style.margin = '2px';
+            pixel.style.height = '100%';
+            pixel.style.width = '100%';
+            pixel.style.flex = '1, 0, auto';
+            pixel.style.justifyContent = 'center';
+            pixel.style.alignItems = 'center';
         }
     };
-   
+};
+
+ 
+
+let pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('pointerover', () => {
+        pixel.style.backgroundColor = 'white'
+    })
+});
+
+pixels.forEach((pixel) => {
+    button.addEventListener('click', () => {   
+        pixel.style.backgroundColor = 'black'
+    })
+});
+
+button.addEventListener('click', () => {
+    grid(0)
+    console.log(grid(0))
+    let newSize = prompt('New grid size?')
+    console.log(newSize)
+        if (newSize != null && newSize < 100) {
+        grid(newSize)} 
+});  
+
